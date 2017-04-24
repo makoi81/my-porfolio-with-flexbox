2a2172add8e7f756162522e63e404457381d0efb
Mahamadou KOITA <koitamahamadou2000@yahoo.fr>
2017-04-24 14:20:49 -0400

    deploying to the internets

---
+++ main.js
@@ -0,0 +1,18 @@
+
+$('.smooth').on('click', function(){
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });   
+    return false;
+});
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
