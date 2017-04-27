9a430063627b7226a94bcb308ad530aaa662afe3
Mahamadou KOITA <koitamahamadou2000@yahoo.fr>
2017-04-27 17:09:38 -0400

    add picture and hovee over

--- main.js
+++ main.js
@@ -6,13 +6,3 @@ $('.smooth').on('click', function(){
     });   
     return false;
 });
-
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
